abe662b909c36b8874de9c9fa8e18c44630757f5
jaleel <jasonlamdarksouls@gmail.com>
2022-06-06 19:20:35 +0200

    Create arithmetic functions
    Created simple arithmetic function (add, subtract, multiply, divide) in
    js file. Tested with console.log

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+console.log(add(4, + 567));
+console.log(subtract(4, + 567));
+console.log(multiply(4, + 567));
+console.log(divide(4, + 567));

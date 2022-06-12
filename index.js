5ed2764308df57e5cc63467112369f98fdb9723f
jaleel <jasonlamdarksouls@gmail.com>
2022-06-12 20:23:35 +0200

    Fuse consecutive digits
    Fuse consecutive digits, ie. those not separated by operator symbols.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const display = document.querySelector(".display");
+let display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.querySelector(".equals-button");
+
+let joinedDigits = 0;
+let chosenNumber = "a";
+let chosenOperator = function() {};
+let answer = 0;
+
+let digitArray = [];
+let numbersArray = [];
+let operatorArray = [];
+
 
 function add(a, b) {
     return a + b;
@@ -16,25 +29,92 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
-    return (operator(a, b));
+function populateDisplay(displayText) {
+    display.innerText += (displayText);
 }
-// pretty sure it's .digit
-const numberButtons = document.querySelectorAll(".digit");
+
+function accessNumber(button) {
+    chosenNumber = button.innerText;
+}
 
 numberButtons.forEach(e => {
+    const number = Number(e.innerText);
     e.addEventListener("click", function() {
-        populateDisplay(e.innerText);
-        displayText = e.innerText;
+        populateDisplay(number);
+        digitArray.push(number);
+    });
+
+    e.addEventListener("click", function() {
+        accessNumber(e);
     });
 });
 
-function myFunction() {
-    alert("why hello");
+operatorButtons.forEach(e => {
+    e.addEventListener("click", function() {
+        fuseDigits();
+    })
+    
+    e.addEventListener("click", function() {
+        populateDisplay(e.innerText);
+    });
+
+    e.addEventListener("click", function() {
+        operate(chosenOperator, digitArray);
+    });
+
+    e.addEventListener("click", function() {
+        accessOperator(e);
+        operatorArray.push(chosenOperator);
+    });
+})
+
+equalsButton.addEventListener("click", function() {
+    clearDisplay();
+    display.innerText = "Result: " + operate(chosenOperator, digitArray);
+    popOperatorArray();
+});
+
+
+
+function accessOperator(button) {
+    const operator = button.innerText;
+    switch(operator) {
+        case "+":
+            chosenOperator = add;
+            console.log("ye");
+            break;
+        case "-":
+            chosenOperator = subtract;
+            break;
+        case "*":
+            chosenOperator = multiply;
+            break;
+        case "/":
+            chosenOperator = divide;
+        console.log(chosenOperator);
+    }
 }
 
-function populateDisplay(displayText) {
-    display.innerText = displayText;
+
+function operate(currentOperator, digitArray) {
+    const output = digitArray.reduce(currentOperator);
+    console.log(output);
+    return output;
 }
 
-console.log("answer is: "+ operate(divide, 8, 8));
+function clearDisplay() {
+    display.innerText = "Result: ";
+}
+
+function popOperatorArray() {
+    operatorArray.shift();
+}
+
+function fuseDigits() {
+        joinedDigits = Number(digitArray.toString().split(",").join(""));
+    }
+
+function pushToNumberArray() {
+    numbersArray.push(joinedDigits);
+}
+

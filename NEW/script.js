6f411837946b81f352f24d3ce232d5f83d9172c2
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-06-19 15:57:08 +0100

    Create Calculator Variables

--- NEW/script.js
+++ NEW/script.js
@@ -13,3 +13,12 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+console.log(add(4,10))
+console.log(subtract(4,10))
+console.log(multiply(4,10))
+console.log(divide(4,10))
+
+let firstNumber = 0;
+let operator; 
+let secondNumber = 0;

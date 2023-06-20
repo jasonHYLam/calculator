caf0e1cdea7c60d8ca872dcead7d2642ed306e51
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-06-20 13:00:34 +0100

    Create Operate Function

--- NEW/script.js
+++ NEW/script.js
@@ -22,3 +22,8 @@ console.log(divide(4,10))
 let firstNumber = 0;
 let operator; 
 let secondNumber = 0;
+
+// calls the above functions depending on value of operator.
+function operate(firstNumber, operator, secondNumber) {
+    switch
+}

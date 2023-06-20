4ac7eccc20c34550d5726cc2b844cad2e1f2ca9f
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-06-20 13:09:24 +0100

    Add Return to Switch

--- NEW/script.js
+++ NEW/script.js
@@ -14,16 +14,21 @@ function divide(a, b) {
     return a/b;
 }
 
-console.log(add(4,10))
-console.log(subtract(4,10))
-console.log(multiply(4,10))
-console.log(divide(4,10))
-
 let firstNumber = 0;
 let operator; 
 let secondNumber = 0;
 
 // calls the above functions depending on value of operator.
 function operate(firstNumber, operator, secondNumber) {
-    switch
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
 }
+
